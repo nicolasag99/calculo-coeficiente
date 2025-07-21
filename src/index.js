6e0f64a991082d801195f6e368f9b8b9ddde7cdf
nicolasag99 <nicolasag1424@gmail.com>
2025-07-21 13:06:13 -0300

    Acrescentei coeficiente com SPF

--- src/index.js
+++ src/index.js
@@ -8,22 +8,26 @@ function calcularCoeficiente() {
     return;
   }
 
-  const i = taxaPercent / 100; // converte para decimal
+  const spf = 0.0304;
+  const i = taxaPercent / 100;
   const coeficiente = (i * Math.pow(1 + i, prazo)) / (Math.pow(1 + i, prazo) - 1);
-//   const coeficiente = i / (1 - Math.pow(1 + i, -prazo));
+  const coeficienteSpf = (spf/prazo) + coeficiente;
 
   document.getElementById("resultado").innerText =
-    `Coeficiente: ${coeficiente.toFixed(10)}`;
+    `Coeficiente: ${coeficiente.toFixed(7)}`;
+
+  document.getElementById("resultadoSpf").innerText =
+  `Coeficiente com SPF: ${coeficienteSpf.toFixed(7)}`;
+
 }
+
+
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         calcularCoeficiente();
     }
     });
-
-
-// Suponha que você financiou R$ 100.000,00 com uma taxa de juros efetiva de 1% ao mês, em 60 meses (5 anos). Usando a fórmula: 
 
 //     Calcular o coeficiente: C = (0.01 * (1 + 0.01)^60) / ((1 + 0.01)^60 - 1) = 0.022244.
 //     Calcular o valor da parcela: Prestação = 100.000 * 0.022244 = R$ 2.224,44.
