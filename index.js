02fc1f28274970f9da9e292f579c6003cb820a60
nicolasag99 <nicolasag1424@gmail.com>
2025-07-20 21:36:33 -0300

    Acrescentando evento keydown

--- index.js
+++ index.js
@@ -16,8 +16,16 @@ function calcularCoeficiente() {
     `Coeficiente: ${coeficiente.toFixed(10)}`;
 }
 
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        calcularCoeficiente();
+    }
+    });
+
 
 // Suponha que você financiou R$ 100.000,00 com uma taxa de juros efetiva de 1% ao mês, em 60 meses (5 anos). Usando a fórmula: 
 
 //     Calcular o coeficiente: C = (0.01 * (1 + 0.01)^60) / ((1 + 0.01)^60 - 1) = 0.022244.
 //     Calcular o valor da parcela: Prestação = 100.000 * 0.022244 = R$ 2.224,44.
+
+
